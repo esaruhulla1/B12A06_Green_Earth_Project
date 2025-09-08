20906014431cd4600f86526110c367a9b08cd3e7
Esa <esaruhulla01@gmail.com>
2025-09-08 20:25:00 +0600

    Show card by category added

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const CategoriesContainer = document.getElementById('categories-container')
 const cardContainer = document.getElementById('card-container')
 
-// All categories
-const loadAllCategories = ()=>{
+// All defould card
+const loadAllCard = ()=>{
     fetch("https://openapi.programming-hero.com/api/plants")
     .then((res)=>res.json())
     .then((data)=>{
-        showAllCategories(data.plants);
+        showAllCard(data.plants);
         
     })
+    .catch((err=>{
+        console.log(err);
+        
+    }))
 }
-const showAllCategories = (plants)=>{
+const showAllCard = (plants)=>{
     plants.forEach(plant=>{
+        
         cardContainer.innerHTML +=`
                 <div class="card bg-white  shadow-sm p-3 ">
                     <figure class="h-48 max-w-80">
@@ -32,11 +37,7 @@ const showAllCategories = (plants)=>{
     })
     
 }
-loadAllCategories()
-
-
-
-
+loadAllCard()
 
 
 //Categories
@@ -48,16 +49,52 @@ const loadCategories = ()=>{
         showCategories(categories)
     })
     .catch((err)=>{
-        console.log(err);
-        
+        console.log(err); 
     })
+
 }
 const showCategories = (categories)=>{
     categories.forEach(category => {
         CategoriesContainer.innerHTML +=`
-                    <h2 class="p-1 pl-2 text-lg rounded-sm hover:bg-[#03a73f] hover:text-white cursor-pointer">${category.category_name}</h2>
+                    <h2 onclick="loadCardByCategory(${category.id})" class="p-1 pl-2 text-lg rounded-sm hover:bg-[#03a73f] hover:text-white cursor-pointer">${category.category_name}</h2>
 
         `  
     });
 }
 loadCategories()
+
+
+//card by Category
+const loadCardByCategory= (id)=>{
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then(res=>res.json())
+    .then(data=>{
+        showCardByCategory(data.plants)
+    })
+    .catch((err=>{
+        console.log(err);     
+    }))  
+}
+const showCardByCategory= (plants)=>{
+    cardContainer.innerHTML = ""
+    plants.forEach((plant)=>{
+    cardContainer.innerHTML +=`
+                <div class="card bg-white  shadow-sm p-3 ">
+                    <figure class="h-48 max-w-80">
+                        <img class="" src="${plant.image}" />
+                    </figure>
+                    <div class="space-y-3">
+                        <h2 class="font-semibold mt-2">${plant.name}</h2>
+                        <p class="text-[#4c545f]  overflow-hidden text-ellipsis line-clamp-3">${plant.description}</p>
+                        <div class="card-actions justify-between">
+                            <div class="bg-[#dcfce7] text-[#15803d] rounded-xl p-1 px-3">${plant.category}</div>
+                            <div class="font-bold">৳${plant.price}</div>
+                        </div>
+                        <button class="btn w-full rounded-2xl bg-[#15803d] text-white">Add to Cart</button>
+                    </div>
+                </div>
+        `
+    })
+
+    
+}
